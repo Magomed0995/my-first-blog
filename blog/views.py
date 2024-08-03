c06a32a3e2d3abd62b4b7c279d17816877f110aa
Muhammad <liki.999.lik@mail.ru>
2024-08-03 11:19:01 +0300

    MySite version2. Add nemplates and first urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return render(request, 'blog/index.html', {})
 # Create your views here.
+
+def categ(request):
+    return HttpResponse('<h1>page1</h1>')
